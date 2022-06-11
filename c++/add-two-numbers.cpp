c66e4231cdb7cc83b252d52ce767520c6fa6dc7d
waliurr <waliurr00@gmail.com>
2022-06-11 17:52:29 +0600

    Create files

---
+++ c++/add-two-numbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter first number: ";
+    cin>>a;
+    cout<<"Enter second number: ";
+    cin>>b;
+    sum = a+b;
+    cout<<"Summation: "<<sum;
+    return 0;
+}
