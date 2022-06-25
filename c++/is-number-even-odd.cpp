cfafbc9208436ee38508c6ce5e157c0194a9892c
waliurr <waliurr00@gmail.com>
2022-06-25 11:30:41 +0600

    create files

---
+++ c++/is-number-even-odd.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+    if(num%2==0){
+        cout<<num<<" is an even number";
+    }
+    else{
+        cout<<num<<" is an odd number";
+    }
+    return 0;
+}
